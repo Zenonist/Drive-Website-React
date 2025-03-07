2c877855e99f84fa04fd6caa8d8b5cd245a058eb
Zenonist <phonarnun@hotmail.com>
2025-03-07 16:39:34 +0700

    feat: Update database schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,30 @@
-import { int, text, singlestoreTable } from "drizzle-orm/singlestore-core";
+import { int, text, index, singlestoreTableCreator, bigint } from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
-  id: int("id").primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
-});
+// NOTE: This helps to avoid table name collisions with other application by prefixing the table name with the application name.
+export const createTable = singlestoreTableCreator(
+  (name) => `GoogleDriveClone_${name}`
+)
+
+export const files = createTable("files_table", {
+  id : bigint({mode: "number", unsigned: true}).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  size: int("size").notNull(),
+  url: text("url").notNull(),
+  parent: bigint("parent", {mode: "number", unsigned: true}).notNull(),
+}, (t) => {
+  return [
+    index("parent_index").on(t.parent)
+  ]
+})
+
+export const folders = createTable("folders_table", {
+  id : bigint({mode: "number", unsigned: true}).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  parent: int("parent"),
+}, (t) => {
+  return [
+    index("parent_index").on(t.parent)
+  ]
+})
+
+// *NOTE - Remove users table because does not use it
