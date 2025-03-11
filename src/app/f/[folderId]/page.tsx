53586275a9966ba6bc4cee81ffa91c50ed059e6d
Zenonist <phonarnun@hotmail.com>
2025-03-11 14:36:57 +0700

    refactor: Move database queries to a separate module and simplify folder retrieval logic

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,43 +1,5 @@
-import { db } from "~/server/db";
-import {
-  files_table as fileSchema,
-  folders_table as folderSchema,
-} from "~/server/db/schema";
 import DriveContents from "../../drive-contents";
-import { eq } from "drizzle-orm";
-
-/**
- * Retrieves all parent folders for a given folder in a hierarchical order.
- * 
- * This function traverses up the folder hierarchy starting from the specified
- * folder ID, collecting all parent folders until it reaches the root folder
- * (which has null as parent).
- * 
- * @async
- * @param {number} folderId - The ID of the folder to get parents for
- * @returns {Promise<Array<Folder>>} An array of parent folders ordered from the direct parent to the root
- * @throws {Error} Throws an error if a parent folder is not found in the database
- */
-async function getAllParents(folderId: number) {
-  const parents = [];
-  let currentFolderId: number | null = folderId;
-  while (currentFolderId !== null) {
-    const folder = await db
-      .selectDistinct()
-      .from(folderSchema)
-      .where(eq(folderSchema.id, currentFolderId)
-    );
-    if (!folder || folder.length === 0) {
-      throw new Error("parent folder not found");
-    }
-    // Reverse the order of the parents array -> This will make the correct order of the directory or path
-    if (folder[0]?.id !== 1) {
-      parents.unshift(folder[0]);
-    }
-    currentFolderId = folder[0]?.parent ?? null;
-  }
-  return parents;
-}
+import { QUERIES } from "~/server/db/queries";
 
 export default async function GoogleDriveClone(props: {
   params: Promise<{ folderId: string }>;
@@ -48,22 +10,18 @@ export default async function GoogleDriveClone(props: {
   if (isNaN(parsedFolderId)) {
     return <div> Invalid folder ID </div>;
   }
-  
-  const foldersPromise = db
-    // NOTE: SQL query -> select * from folderSchema where parent == parsedFolderId
-    .select()
-    .from(folderSchema)
-    // eq = equal
-    .where(eq(folderSchema.parent, parsedFolderId));
-  
-  const filesPromise = db
-    .select()
-    .from(fileSchema)
-    .where(eq(fileSchema.parent, parsedFolderId));
 
-  const parentsPromise = getAllParents(parsedFolderId);
-  
-  const [folders, files, parents] = await Promise.all([foldersPromise, filesPromise, parentsPromise]);
+  const [folders, files, parents] = await Promise.all([
+    QUERIES.getFolders(parsedFolderId),
+    QUERIES.getFiles(parsedFolderId),
+    QUERIES.getAllParentsForFolder(parsedFolderId),
+  ]);
 
-  return <DriveContents files={files} folders={folders} parents={parents.filter(Boolean)}></DriveContents>;
+  return (
+    <DriveContents
+      files={files}
+      folders={folders}
+      parents={parents}
+    ></DriveContents>
+  );
 }
---
+++ src/server/db/queries.ts
@@ -0,0 +1,59 @@
+import "server-only";
+
+import { db } from "~/server/db";
+import {
+    files_table as fileSchema,
+    folders_table as folderSchema,
+} from "~/server/db/schema";
+import { eq } from "drizzle-orm";
+
+export const QUERIES = {
+    /**
+     * Retrieves all parent folders for a given folder in a hierarchical order.
+     * 
+     * This function traverses up the folder hierarchy starting from the specified
+     * folder ID, collecting all parent folders until it reaches the root folder
+     * (which has null as parent).
+     * 
+     * @async
+     * @param {number} folderId - The ID of the folder to get parents for
+     * @returns {Promise<Array<Folder>>} An array of parent folders ordered from the direct parent to the root
+     * @throws {Error} Throws an error if a parent folder is not found in the database
+     */
+    getAllParentsForFolder: async function (folderId: number) {
+        const parents = [];
+        let currentFolderId: number | null = folderId;
+        while (currentFolderId !== null) {
+            const folder = await db
+                .selectDistinct()
+                .from(folderSchema)
+                .where(eq(folderSchema.id, currentFolderId)
+                );
+            if (!folder || folder.length === 0) {
+                throw new Error("parent folder not found");
+            }
+            // Reverse the order of the parents array -> This will make the correct order of the directory or path
+            if (folder[0]?.id !== 1) {
+                parents.unshift(folder[0]);
+            }
+            currentFolderId = folder[0]?.parent ?? null;
+        }
+        return parents;
+    },
+
+    getFolders: function (folderId: number) {
+        return db
+            // NOTE: SQL query -> select * from folderSchema where parent == parsedFolderId
+            .select()
+            .from(folderSchema)
+            // eq = equal
+            .where(eq(folderSchema.parent, folderId));
+    },
+
+    getFiles: function (folderId: number) {
+        return db
+            .select()
+            .from(fileSchema)
+            .where(eq(fileSchema.parent, folderId));
+    }
+}
