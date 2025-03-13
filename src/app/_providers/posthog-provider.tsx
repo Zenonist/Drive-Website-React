2b9452c18927aea2f531f6ed28ba5ba838e94354
Zenonist <phonarnun@hotmail.com>
2025-03-13 17:11:47 +0700

    feat: Add posthog with reverse Proxy

--- src/app/_providers/posthog-provider.tsx
+++ src/app/_providers/posthog-provider.tsx
@@ -1,57 +1,69 @@
 // app/providers.tsx
-'use client'
+"use client";
 
-import { usePathname, useSearchParams } from "next/navigation"
-import { useEffect, Suspense } from "react"
-import { usePostHog } from 'posthog-js/react'
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect, Suspense } from "react";
+import { usePostHog } from "posthog-js/react";
 
-import posthog from 'posthog-js'
-import { PostHogProvider as PHProvider } from 'posthog-js/react'
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
+import { useAuth, useUser } from "@clerk/nextjs";
+import dynamicLoader from "next/dynamic";
+
+// This allows us to load PostHog dynamically, so that it doesn't load on the server.
+const SuspendedPostHogPageView = dynamicLoader(
+  () => Promise.resolve({ default: PostHogPageView }),
+  {
+    // This component is set to not render during server-side rendering (SSR: false)
+    ssr: false,
+  },
+);
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-      capture_pageview: false // Disable automatic pageview capture, as we capture manually
-    })
-  }, [])
+      api_host: "/ingest",
+      ui_host: "https://us.posthog.com"
+    });
+  }, []);
 
   return (
     <PHProvider client={posthog}>
       <SuspendedPostHogPageView />
       {children}
     </PHProvider>
-  )
+  );
 }
 
 function PostHogPageView() {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-  const posthog = usePostHog()
+  const userInfo = useUser();
+  const posthog = usePostHog();
+
+  // Identify user
+  useEffect(() => {
+    if (userInfo.user?.id) {
+      posthog.identify(userInfo.user?.id, {
+        email: userInfo.user?.emailAddresses[0]?.emailAddress,
+        name: userInfo.user?.fullName,
+      });
+    } else {
+      posthog.reset();
+    }
+  }, [posthog, userInfo.user?.id]);
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   // Track pageviews
   useEffect(() => {
     if (pathname && posthog) {
-      let url = window.origin + pathname
+      let url = window.origin + pathname;
       if (searchParams.toString()) {
         url = url + "?" + searchParams.toString();
       }
 
-      posthog.capture('$pageview', { '$current_url': url })
+      posthog.capture("$pageview", { $current_url: url });
     }
-  }, [pathname, searchParams, posthog])
+  }, [pathname, searchParams, posthog]);
 
-  return null
-}
-
-// Wrap PostHogPageView in Suspense to avoid the useSearchParams usage above
-// from de-opting the whole app into client-side rendering
-// See: https://nextjs.org/docs/messages/deopted-into-client-rendering
-function SuspendedPostHogPageView() {
-  return (
-    <Suspense fallback={null}>
-      <PostHogPageView />
-    </Suspense>
-  )
+  return null;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,7 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
-import {
-  ClerkProvider
-} from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
 import { PostHogProvider } from "./_providers/posthog-provider";
 
 export const metadata: Metadata = {
@@ -16,15 +14,15 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <ClerkProvider>
-      <PostHogProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
-        <body>{children}</body>
+        <body>
+          <PostHogProvider>{children}</PostHogProvider>
+        </body>
       </html>
-      </PostHogProvider>
     </ClerkProvider>
-  )
+  );
 }
