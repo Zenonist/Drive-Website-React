929a31806283a0874df03badd00fc43835d03939
Zenonist <phonarnun@hotmail.com>
2025-03-17 14:24:16 +0700

    feat: Add "Create folder" function

--- src/server/actions.ts
+++ src/server/actions.ts
@@ -2,7 +2,7 @@
 
 import { and, eq } from "drizzle-orm";
 import { db } from "~/server/db";
-import { files_table } from "~/server/db/schema";
+import { files_table, folders_table } from "~/server/db/schema";
 import { auth } from "@clerk/nextjs/server";
 import { UTApi } from "uploadthing/server";
 import { cookies } from "next/headers";
@@ -43,4 +43,23 @@ export async function deleteFile(fileId: number) {
     return {
         success: true,
     };
+}
+
+export async function createFolder(name: string, parentId: number) {
+    const session = await auth();
+    if (!session.userId) {
+        throw new Error("Unauthorized");
+    }
+    const folder = await db.insert(folders_table).values({
+        name,
+        parent: parentId,
+        ownerId: session.userId,
+    });
+    
+    // Update the cookie to force a refresh
+    const c = await cookies();
+    c.set("force-refresh", JSON.stringify(Math.random()));
+    return {
+        success: true
+    }
 }
